87ce0006f0987ba9304e95525aeac6f2cf34bcb0
Siekpheng <forsaken310803@gmail.com>
2026-01-09 10:21:53 +0700

    camera working

--- hcr5_ws/src/hcr5_description/launch/gazebo.launch.py
+++ hcr5_ws/src/hcr5_description/launch/gazebo.launch.py
@@ -28,7 +28,7 @@ def generate_launch_description():
         PythonLaunchDescriptionSource(
             os.path.join(pkg_ros_gz_sim, "launch", "gz_sim.launch.py")
         ),
-        launch_arguments={'gz_args': '-r empty.sdf'}.items(),
+        launch_arguments={'gz_args': '-r sensors.sdf'}.items(),
     )
 
     # 5. The Clock Bridge (CRITICAL: Added back into the logic)
