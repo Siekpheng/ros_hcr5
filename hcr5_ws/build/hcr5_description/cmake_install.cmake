a595644cc51da9b4a3d961bd9c25132003d36c0c
Siekpheng <forsaken310803@gmail.com>
2026-01-15 09:13:30 +0700

    Refactor HCR5 URDF and materials: remove obsolete files, update links, and enhance camera configurations
    - Deleted the old hcr5.xacro and materials.xacro files, replacing them with symlinks to the new locations.
    - Removed hcr5.trans file as it was no longer needed.
    - Updated the table_link visual material to grey and adjusted dimensions for camera_pole1.
    - Enhanced camera_link1 sensor settings for improved resolution and update rate.
    - Added commented-out sections for potential future gripper joint and link configurations.

--- hcr5_ws/build/hcr5_description/cmake_install.cmake
+++ hcr5_ws/build/hcr5_description/cmake_install.cmake
@@ -43,72 +43,7 @@ if(NOT DEFINED CMAKE_OBJDUMP)
 endif()
 
 if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/hcr5_description" TYPE DIRECTORY FILES
-    "/home/forsaken/Documents/ros_hcr5/hcr5_ws/src/hcr5_description/launch"
-    "/home/forsaken/Documents/ros_hcr5/hcr5_ws/src/hcr5_description/urdf"
-    "/home/forsaken/Documents/ros_hcr5/hcr5_ws/src/hcr5_description/meshes"
-    "/home/forsaken/Documents/ros_hcr5/hcr5_ws/src/hcr5_description/rviz"
-    "/home/forsaken/Documents/ros_hcr5/hcr5_ws/src/hcr5_description/config"
-    )
-endif()
-
-if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/ament_index/resource_index/package_run_dependencies" TYPE FILE FILES "/home/forsaken/Documents/ros_hcr5/hcr5_ws/build/hcr5_description/ament_cmake_index/share/ament_index/resource_index/package_run_dependencies/hcr5_description")
-endif()
-
-if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/ament_index/resource_index/parent_prefix_path" TYPE FILE FILES "/home/forsaken/Documents/ros_hcr5/hcr5_ws/build/hcr5_description/ament_cmake_index/share/ament_index/resource_index/parent_prefix_path/hcr5_description")
-endif()
-
-if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/hcr5_description/environment" TYPE FILE FILES "/opt/ros/humble/share/ament_cmake_core/cmake/environment_hooks/environment/ament_prefix_path.sh")
-endif()
-
-if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/hcr5_description/environment" TYPE FILE FILES "/home/forsaken/Documents/ros_hcr5/hcr5_ws/build/hcr5_description/ament_cmake_environment_hooks/ament_prefix_path.dsv")
-endif()
-
-if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/hcr5_description/environment" TYPE FILE FILES "/opt/ros/humble/share/ament_cmake_core/cmake/environment_hooks/environment/path.sh")
-endif()
-
-if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/hcr5_description/environment" TYPE FILE FILES "/home/forsaken/Documents/ros_hcr5/hcr5_ws/build/hcr5_description/ament_cmake_environment_hooks/path.dsv")
-endif()
-
-if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/hcr5_description" TYPE FILE FILES "/home/forsaken/Documents/ros_hcr5/hcr5_ws/build/hcr5_description/ament_cmake_environment_hooks/local_setup.bash")
-endif()
-
-if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/hcr5_description" TYPE FILE FILES "/home/forsaken/Documents/ros_hcr5/hcr5_ws/build/hcr5_description/ament_cmake_environment_hooks/local_setup.sh")
-endif()
-
-if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/hcr5_description" TYPE FILE FILES "/home/forsaken/Documents/ros_hcr5/hcr5_ws/build/hcr5_description/ament_cmake_environment_hooks/local_setup.zsh")
-endif()
-
-if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/hcr5_description" TYPE FILE FILES "/home/forsaken/Documents/ros_hcr5/hcr5_ws/build/hcr5_description/ament_cmake_environment_hooks/local_setup.dsv")
-endif()
-
-if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/hcr5_description" TYPE FILE FILES "/home/forsaken/Documents/ros_hcr5/hcr5_ws/build/hcr5_description/ament_cmake_environment_hooks/package.dsv")
-endif()
-
-if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/ament_index/resource_index/packages" TYPE FILE FILES "/home/forsaken/Documents/ros_hcr5/hcr5_ws/build/hcr5_description/ament_cmake_index/share/ament_index/resource_index/packages/hcr5_description")
-endif()
-
-if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/hcr5_description/cmake" TYPE FILE FILES
-    "/home/forsaken/Documents/ros_hcr5/hcr5_ws/build/hcr5_description/ament_cmake_core/hcr5_descriptionConfig.cmake"
-    "/home/forsaken/Documents/ros_hcr5/hcr5_ws/build/hcr5_description/ament_cmake_core/hcr5_descriptionConfig-version.cmake"
-    )
-endif()
-
-if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/hcr5_description" TYPE FILE FILES "/home/forsaken/Documents/ros_hcr5/hcr5_ws/src/hcr5_description/package.xml")
+  include("/home/forsaken/Documents/ros_hcr5/hcr5_ws/build/hcr5_description/ament_cmake_symlink_install/ament_cmake_symlink_install.cmake")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
